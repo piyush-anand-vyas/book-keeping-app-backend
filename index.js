b9240fb4d177126fd40bae681f461e8b1dc6d8eb
piyush-anand-vyas <piyush__vyas@outlook.com>
2022-01-31 12:56:47 +0530

    Port modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const connectToMongo = require('./db')
 connectToMongo()
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 const cors = require('cors')
 
 app.use(express.json())
